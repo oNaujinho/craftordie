92c6e0d723b7a375ae59debaff65d5dcc745ebfc
Juan Braz <jtelink1000@gmail.com>
2021-07-08 05:47:40 -0300

    Colocando os arquivos

---
+++ src/com/gcstudios/main/Game.java
@@ -0,0 +1,264 @@
+package com.gcstudios.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.JFrame;
+
+
+import com.gcstudios.entities.Entity;
+import com.gcstudios.entities.Player;
+import com.gcstudios.graficos.Spritesheet;
+import com.gcstudios.graficos.UI;
+import com.gcstudios.world.World;
+
+public class Game extends Canvas implements Runnable,KeyListener,MouseListener,MouseMotionListener{
+
+	private static final long serialVersionUID = 1L;
+	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 640;
+	public static final int SCALE = 1;
+	
+	private BufferedImage image;
+	
+	public static World world;
+	public static List<Entity> entities;
+	public static Spritesheet spritesheet;
+	public static Player player;
+
+	public UI ui;
+	
+	public static Inventory inventory;
+	
+	public static int pxSize = 64;
+	public static int FPS = 0;
+	public static boolean debug_mode = true;
+	
+	public Game(){
+		addKeyListener(this);
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		setPreferredSize(new Dimension(WIDTH*SCALE,HEIGHT*SCALE));
+		initFrame();
+		image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+		
+		//Inicializando objetos.
+		spritesheet = new Spritesheet("/spritesheet.png");
+		entities = new ArrayList<Entity>();
+		player = new Player(WIDTH/2 - 30,HEIGHT/2,16,16,1.4,Entity.PLAYER_SPRITE_RIGHT[0]);
+		world = new World();
+		ui = new UI();
+		inventory = new Inventory();
+		
+		entities.add(player);
+		
+	}
+	
+	public void initFrame(){
+		frame = new JFrame("Minecraft");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	public synchronized void start(){
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String args[]){
+		Game game = new Game();
+		game.start();
+	}
+	
+	public void tick(){
+		
+
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.tick();
+		}
+		
+		inventory.tick();
+		
+	}
+	
+
+
+	
+	public void render(){
+		BufferStrategy bs = this.getBufferStrategy();
+		if(bs == null){
+			this.createBufferStrategy(2);
+			return;
+		}
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(122,102,255));
+		g.fillRect(0, 0,WIDTH,HEIGHT);
+		g.dispose();
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0,WIDTH*SCALE,HEIGHT*SCALE,null);
+		/*Renderização do jogo*/
+		//Graphics2D g2 = (Graphics2D) g;
+		world.render(g);
+		Collections.sort(entities,Entity.nodeSorter);
+		for(int i = 0; i < entities.size(); i++) {
+			Entity e = entities.get(i);
+			e.render(g);
+		}
+		inventory.render(g);
+		ui.render(g);
+		/***/
+
+		
+		bs.show();
+	}
+	
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+		requestFocus();
+		while(isRunning){
+			long now = System.nanoTime();
+			delta+= (now - lastTime) / ns;
+			lastTime = now;
+			if(delta >= 1) {
+				tick();
+				render();
+				frames++;
+				delta--;
+			}
+			
+			if(System.currentTimeMillis() - timer >= 1000){
+				Game.FPS = frames;
+				frames = 0;
+				timer+=1000;
+			}
+			
+		}
+		
+		stop();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = true;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			player.jump = true;
+		}
+		if ((e.getKeyCode() > 48 && e.getKeyCode() <= 57)) { 
+			  // 0-9 only
+			int key = (e.getKeyCode()-50)+2;
+			inventory.selected = key-1;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			inventory.selected = -1;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = false;
+		}
+		
+
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if(e.getButton() == MouseEvent.BUTTON1) {
+			inventory.isPressed = true;
+		} else if(e.getButton() == MouseEvent.BUTTON3) {
+			inventory.isPlaceItem = true;
+		}
+		
+		inventory.mx = e.getX();
+		inventory.my = e.getY();	
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+	
+	}
+
+	
+}
